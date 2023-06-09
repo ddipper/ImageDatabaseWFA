130d04e713a7742641580e1bf3bdad5dde491e17
Dmitry <dimonbyt@gmail.com>
2023-06-09 16:10:30 +0300

    Add project files.

---
+++ ImageDatabaseWFA/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ImageDatabaseWFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureSet = new System.Windows.Forms.PictureBox();
+            this.pictureView = new System.Windows.Forms.PictureBox();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.buttonSetPhoto = new System.Windows.Forms.Button();
+            this.buttonVIew = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureSet
+            // 
+            this.pictureSet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.pictureSet.Location = new System.Drawing.Point(133, 61);
+            this.pictureSet.Name = "pictureSet";
+            this.pictureSet.Size = new System.Drawing.Size(162, 174);
+            this.pictureSet.TabIndex = 0;
+            this.pictureSet.TabStop = false;
+            // 
+            // pictureView
+            // 
+            this.pictureView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.pictureView.Location = new System.Drawing.Point(416, 61);
+            this.pictureView.Name = "pictureView";
+            this.pictureView.Size = new System.Drawing.Size(162, 174);
+            this.pictureView.TabIndex = 1;
+            this.pictureView.TabStop = false;
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.buttonSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSelect.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSelect.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonSelect.Location = new System.Drawing.Point(143, 285);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(141, 44);
+            this.buttonSelect.TabIndex = 2;
+            this.buttonSelect.Text = "Select";
+            this.buttonSelect.UseVisualStyleBackColor = false;
+            // 
+            // buttonSetPhoto
+            // 
+            this.buttonSetPhoto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.buttonSetPhoto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSetPhoto.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSetPhoto.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonSetPhoto.Location = new System.Drawing.Point(133, 381);
+            this.buttonSetPhoto.Name = "buttonSetPhoto";
+            this.buttonSetPhoto.Size = new System.Drawing.Size(162, 44);
+            this.buttonSetPhoto.TabIndex = 3;
+            this.buttonSetPhoto.Text = "Set image";
+            this.buttonSetPhoto.UseVisualStyleBackColor = false;
+            // 
+            // buttonVIew
+            // 
+            this.buttonVIew.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.buttonVIew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonVIew.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonVIew.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonVIew.Location = new System.Drawing.Point(416, 381);
+            this.buttonVIew.Name = "buttonVIew";
+            this.buttonVIew.Size = new System.Drawing.Size(162, 44);
+            this.buttonVIew.TabIndex = 4;
+            this.buttonVIew.Text = "View";
+            this.buttonVIew.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(160, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select image:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(150, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Add image toDB";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(435, 356);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Add image toDB";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(413, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Select id in DB";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.textBoxId.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBoxId.Location = new System.Drawing.Point(532, 282);
+            this.textBoxId.MaxLength = 5;
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(46, 26);
+            this.textBoxId.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(39)))), ((int)(((byte)(231)))));
+            this.ClientSize = new System.Drawing.Size(698, 479);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonVIew);
+            this.Controls.Add(this.buttonSetPhoto);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.pictureView);
+            this.Controls.Add(this.pictureSet);
+            this.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureSet;
+        private System.Windows.Forms.PictureBox pictureView;
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.Button buttonSetPhoto;
+        private System.Windows.Forms.Button buttonVIew;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxId;
+    }
+}
+
